694ee985d60f35d923330f5ea4cc578465ba13bf
Anna Luiza <anna.l.costa6@aluno.senai.br>
2026-09-09 15:50:47 -0300

    criada as pastas para produtos

---
+++ src/app/components/produto/produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-produto',
+  styleUrl: './produto.css',
+  templateUrl: './produto.html',
+})
+export class Produto {}
