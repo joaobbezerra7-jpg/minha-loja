f7169749da05025d5fed6e727f0e58a4ac3de556
Daniel <daniel.h.nunes@aluno.senai.br>
2026-09-09 16:12:02 -0300

    fazeno a interface da loja

--- src/app/loja/loja.component.ts
+++ src/app/loja/loja.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-loja',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './loja.component.html',
   styleUrl: './loja.component.css'
 })
 export class LojaComponent {
-
+  title = 'minha-loja'
 }
