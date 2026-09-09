e400c2242ad9cffd84363531f6cfb16d69b743c6
erisson <erisson.o@aluno.senai.br>
2026-09-09 14:23:40 -0300

    adicionado primeiro commit cd.ts

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,11 +1,29 @@
+
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
 export class Cadastro {
 
+  formCadastro = this.fb.group({
+    nome: [''],
+    cpf: [''],
+    email: [''],
+    dataNascimento: [''],
+    cep: ['']
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  salvar() {
+    console.log(this.formCadastro.value);
+  }
+
 }
+
