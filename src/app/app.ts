4c054921c30ab8cb29d5fd31679e0cd2f9e913a1
Daniel <daniel.h.nunes@aluno.senai.br>
2026-09-10 16:38:57 -0300

    coloquei a logo no global para aparecer em todos os component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { LojaComponent } from './loja/loja.component';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink,],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+
+export class App {
+
+}
