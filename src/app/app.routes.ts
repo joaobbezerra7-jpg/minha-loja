538a0b44a80407869e097d654cca89b3b0c3ce8b
cristian <cristian.h.santos6@aluno.senai.br>
2026-09-10 13:14:14 -0300

    retirando import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './componentes/cadastro/cadastro';
-import { ListaCadastros } from './componentes/lista-cadastros/lista-cadastros';
 
 export const routes: Routes = [
     {path:"",redirectTo: 'cadastro',pathMatch:'full' },
     {path:'cadastro',component:CadastroComponent },
-    {path:'lista-cadastro',component:ListaCadastros },
+
 ];
