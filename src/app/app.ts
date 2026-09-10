d634a54967b9fa6065e578302fceba271348cf91
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-09-10 14:46:41 -0300

    ..

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { LojaComponent } from './loja/loja.component';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, LojaComponent],
-=======
-=======
-import { LojaComponent } from './loja/loja.component';
->>>>>>> a69e0393f8790b775ac6c8681d6957886439822d
 import { ProdutoComponent } from './produto/produto';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  imports: [
-    RouterOutlet,
-    ProdutoComponent
-  ],
->>>>>>> 9a11c85025001bd50b1e2fb3f7f2edb2106492b4
-=======
-  imports: [RouterOutlet, ProdutoComponent, LojaComponent],
->>>>>>> a69e0393f8790b775ac6c8681d6957886439822d
+  imports: [RouterOutlet, LojaComponent, ProdutoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
-
 })
-
 export class App {
 }
