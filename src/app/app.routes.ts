2eeb029c79f35b08bcc3ebfab59f319a6294b7f4
vivian <vivian.g.souza6@aluno.senai.br>
2026-09-10 16:42:47 -0300

    alterado carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+
+export const routes: Routes = [
+  {
+    path: 'carrinho',
+    component: CarrinhoComponent
+  },
+  {
+    path: '',
+    redirectTo: 'carrinho',
+    pathMatch: 'full'
+  }
+];
