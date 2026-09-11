1b2406a50356014da0a2d6862578f0aebd09aca5
Daniel <daniel.h.nunes@aluno.senai.br>
2026-09-11 13:16:27 -0300

    apenas tirei linhas sem nada

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
