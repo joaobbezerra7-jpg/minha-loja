7041d7be8837d75b6c0209de11ba929f03989b24
vivian <vivian.g.souza6@.aluno.senai.br>
2026-09-10 22:11:00 -0300

    mesclando branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'carrinho',
+    component: CarrinhoComponent
+  },
+  {
+    path: '',
+    redirectTo: 'carrinho',
+    pathMatch: 'full'
+  }
+];
